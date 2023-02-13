a92840de57da4455639886a8c3f4610605be70cf
df <df>
2023-02-13 13:48:11 +0700

    hide GUI for build script

--- Export PCK.bat
+++ Export PCK.bat
@@ -3,7 +3,6 @@
 echo This script will create a translation PCK file for Paper Lily with the files located in the "package" folder.
 echo Make sure the Export Templates have been installed in the Godot Engine!
 echo.
-pause
 
 rem Remove the old file
 del translation_vi.pck
@@ -12,7 +11,7 @@ rem Move to the package folder
 cd package
 
 rem Export the PCK file with the translation assets
-..\godot.exe --export "Pack Exporter" ../translation_vi.pck
+..\godot.exe --no-window --export "Pack Exporter" ../translation_vi.pck
 
 rem Add the definition files to the previously created PCK
 ..\godotpcktool.exe ..\translation_vi.pck -a a definitions
